ed6c7f9658c6a1d676f4883966aa1fcb26cbef54
ron <ron@MSI>
2016-05-18 00:03:08 +0300

    הכנסתי שינויים בקוד תראה בשורה 8 יש for חדש

--- src/test1/test1.java
+++ src/test1/test1.java
@@ -5,6 +5,10 @@ public class test1 {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("tasting...");
+		for(int i = 0 ; i< 30 ; i++)
+		{
+			System.out.println("tasting...");
+		}
 
 	}
 
